8900f93ffe7304dcda8be299f7cf5d6b845360d3
AJCamposss <adrian.campos869@gmail.com>
2023-10-30 13:57:41 -0700

    Added an extra little section and started on JavaScript

---
+++ scripts/cost.js
@@ -0,0 +1,15 @@
+ window.onload = init;
+
+function init () {
+    const totalCost = document.getElementById("totalCost");
+
+    totalCost.onsubmit = getTotalCost; 
+}
+
+function totalCost(event) {
+    
+    let fullName = document.getElementById("fullNameInput");
+    let email = document.getElementById("emailInput");
+    let checkInDate = document.getElementById("")
+
+}
