941840aa72bee2f35e19142902a8392d6fe4d050
AJCamposss <adrian.campos869@gmail.com>
2023-10-30 14:00:09 -0700

    Changed the id for CheckIn Date and continued with JavaScript

--- scripts/cost.js
+++ scripts/cost.js
@@ -10,6 +10,7 @@ function totalCost(event) {
     
     let fullName = document.getElementById("fullNameInput");
     let email = document.getElementById("emailInput");
+    let phoneNumber = document.getElementById("phoneNumberInput");
     let checkInDate = document.getElementById("")
 
 }
